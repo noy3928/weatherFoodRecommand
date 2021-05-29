adfd865dd7a22455f490863523b709984bcfbe60
SeungBinKim <seungbin0508@gmail.com>
2021-05-30 01:30:38 +0900

    Completed the basic structure of recommend.js

--- static/assets/recommend.js
+++ static/assets/recommend.js
@@ -1,51 +1,50 @@
 (async function () {
 
-	// Initiate recommend function
+	// Initiating recommend function
 
-	// 1. initialize food database
+	// 1. Initializes food database
 	const foods = await fetch('assets/foodData.json')
 	.then(response => response.json())
 	.then(data => data)
 
-	function randomSelect(arr, n = 3) {
-		console.log(n)
-		return arr
+	// 2. Filter
+	const condition = function (food) {
+		return true
+	}
+
+	function selectRandom(arr, n = 3) {
+		return
 	}
 
 	// 2. Creates recommend function
 	const recommend = await (function() {
-
 		/*
-		previous search results should be stroed in localStorage.
+		previous search results should be saved in localStorage
 		*/
+
 		return function () {
-			function condition(food) {
-				return true
-			}
 			const location = document.querySelector('.city-name').innerText
 			const weather = ''//getWeather(location)
-			console.log('검색 값이 입력되었습니다.')
+			console.log('검색이 감지되었습니다.')
 			console.log('검색의 입력값은 다음과 같습니다.', document.querySelector('#searchbar').value)
 			console.log('현재 불러온 음식 db의 내용입니다.')
 			console.table(foods)
 			console.log('검색된 입력값의 날씨 정보입니다.')
 			console.table(weather)
 
-			const selectedFoods = foods.filter(condition)
-			console.log('음식 db 중 날씨 조건에 해당하는 항목입니다.')
-			console.table(selectedFoods)
+			const filteredFoods = foods.filter(condition)
+			console.log('조건에 맞는 음식 목록입니다.')
+			console.table(filteredFoods)
 
-			// 2.1 Returns randomly selected n elements. Default n is 3.
-			const result = randomSelect(selectedFoods, undefined)
+			// selects default three elements from the array
+			const result = selectRandom(filteredFoods, undefined)
+			console.log('결과입니다.')
+			console.table(result)
 
-			return result
 		}
 	})()
 
 	// 3. attach to HTML
 	const searchForm = document.querySelector('form.search')
 	searchForm.addEventListener('submit', recommend)
-
-
-	console.log('추천 시스템이 준비되었습니다.')
 })()
