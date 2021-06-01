e561405406110ff4648ba1e1ef9f98b0a0167140
SeungBinKim <seungbin0508@gmail.com>
2021-06-01 17:56:06 +0900

    test

--- static/assets/showResult.js
+++ static/assets/showResult.js
@@ -1,6 +1,7 @@
 function showResult (cityName, weather, foods) {
 	// 안녕하세요!
 	//네  저도 안녕하세요
+	// 저도 반가워요
 	const $ = document.querySelector.bind(document)
 	// Commonly used DOM elements.
 	const $menus = document.querySelector('.menus')
