d4d7dfed66a3e830373cdc47bf762f55a79a5635
noy3928@naver.com <noyechan@noyechan-ui-MacBook-Pro.local>
2021-06-01 13:44:05 +0900

    image slide structure

---
+++ static/JS/foodSlide.js
@@ -0,0 +1,92 @@
+var slides = $(".menu").toArray(), //객체를 배열로 만들어주는 매서드.
+  active = 0,
+  next = 1,
+  prev = slides.length - 1,
+  activePosition = 150;
+spacing = 210;
+
+var positions = {
+  ACTIVE: activePosition,
+  PREV: activePosition - spacing,
+  NEXT: activePosition + spacing,
+  UP: activePosition - spacing * 2,
+  DOWN: activePosition + spacing * 2,
+};
+
+function setUpSlides() {
+  TweenMax.set(slides, { top: positions.DOWN });
+  TweenMax.set(slides[active], { top: positions.ACTIVE });
+  TweenMax.set(slides[next], { top: positions.NEXT });
+  TweenMax.set(slides[prev], { top: positions.PREV });
+}
+
+function animateSlides(isNext) {
+  var onDeck,
+    currentSlides = [];
+
+  if (isNext) {
+    onDeck = (next + 1) % slides.length;
+    currentSlides = [
+      slides[prev],
+      slides[active],
+      slides[next],
+      slides[onDeck],
+    ];
+    TweenMax.set(slides[onDeck], { top: positions.DOWN });
+  } else {
+    onDeck = prev - 1 < 0 ? slides.length - 1 : prev - 1;
+    currentSlides = [
+      slides[next],
+      slides[active],
+      slides[prev],
+      slides[onDeck],
+    ];
+    TweenMax.set(slides[onDeck], { top: positions.UP });
+  }
+
+  for (var i = 0; i < currentSlides.length; i++) {
+    var top;
+
+    switch (i) {
+      case 0:
+        top = isNext ? positions.UP : positions.DOWN;
+        break;
+      case 1:
+        top = isNext ? positions.PREV : positions.NEXT;
+        break;
+      case 2:
+        top = positions.ACTIVE;
+        break;
+      case 3:
+        top = isNext ? positions.NEXT : positions.PREV;
+        break;
+      default:
+        break;
+    }
+    TweenMax.to(currentSlides[i], 1, {
+      top: top,
+      // ease: Strong.easeInOut,
+      ease: Quint.easeInOut,
+      delay: i * 0.1,
+    });
+  }
+  if (isNext) {
+    prev = active;
+    active = next;
+    next = onDeck;
+  } else {
+    next = active;
+    active = prev;
+    prev = onDeck;
+  }
+}
+
+setUpSlides();
+
+$(".next").on("click", function () {
+  animateSlides(true);
+});
+
+$(".prev").on("click", function () {
+  animateSlides(false);
+});
--- static/assets/showResult.js
+++ static/assets/showResult.js
@@ -1,30 +1,24 @@
-function showResult (cityName, weather, foods) {
-		function showCity() {
-
-		}
-		function showWeather() {
-
-		}
-		function showFoods() {
-			const result = new DocumentFragment()
-			for (let i = 0; i < 3; i++) {
-				const menuDiv = document.createElement('div')
-				menuDiv.setAttribute('class', 'menu')
-				result.appendChild(menuDiv)
-			}
-			const menusDiv = document.querySelector('.menus')
-			console.log(menusDiv)
-			document.querySelector('.menus').appendChild(result)
-
-		}
-		try {
-			showCity()
-			showWeather()
-			showFoods()
-			return true
-		}
-		catch (e) {
-			console.error(e)
-			return false
-		}
+function showResult(cityName, weather, foods) {
+  function showCity() {}
+  function showWeather() {}
+  function showFoods() {
+    const result = new DocumentFragment();
+    for (let i = 0; i < 3; i++) {
+      const menuDiv = document.createElement("div");
+      menuDiv.setAttribute("class", "menu");
+      result.appendChild(menuDiv);
+    }
+    const menusDiv = document.querySelector(".menus");
+    console.log(menusDiv);
+    document.querySelector(".menus").appendChild(result);
+  }
+  try {
+    showCity();
+    showWeather();
+    // showFoods()
+    return true;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
 }
