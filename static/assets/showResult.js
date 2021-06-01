aca5e4641eae90a85e050fbaf1c239e19bfdf09c
SeungBinKim <seungbin0508@gmail.com>
2021-06-01 13:07:03 +0900

    Made constants DOM elements of showCity.

--- static/assets/showResult.js
+++ static/assets/showResult.js
@@ -1,29 +1,42 @@
 function showResult (cityName, weather, foods) {
-		function showCity() {
 
-		}
-		function showWeather() {
+	// Commonly used DOM elements.
+	const $menus = document.querySelector('.menus')
+	function clearPreviousResults() {
+		$menus.innerHTML = ''
+	}
 
+	function showCity() {
+		const $imgBox = document.querySelector('.img_box')
+		const $cityImage = document.querySelector('.city-image')
+		const $cityName = document.querySelector('.city-name')
+	}
+
+	function showWeather() {
+
+	}
+
+	function showFoods(foods) {
+		const result = new DocumentFragment()
+		for (let i = 0; i < 3; i++) {
+			const menuDiv = document.createElement('div')
+			menuDiv.setAttribute('class', 'menu')
+			result.appendChild(menuDiv)
 		}
-		function showFoods() {
-			const result = new DocumentFragment()
-			for (let i = 0; i < 3; i++) {
-				const menuDiv = document.createElement('div')
-				menuDiv.setAttribute('class', 'menu')
-				result.appendChild(menuDiv)
-			}
-			const menusDiv = document.querySelector('.menus')
-			console.log(menusDiv)
-			document.querySelector('.menus').appendChild(result)
-		}
-		try {
-			showCity()
-			showWeather()
-			showFoods()
-			return true
-		}
-		catch (e) {
-			console.error(e)
-			return false
-		}
+
+		// Returns empty divs if there's no foods to show. Else continue.
+		if (foods === undefined) $menus.appendChild(result)
+
+	}
+	try {
+		clearPreviousResults()
+		showCity()
+		showWeather()
+		showFoods()
+		return true
+	}
+	catch (e) {
+		console.error(e)
+		return false
+	}
 }
