83dec753b53bd384a16fbfcc1cd5b793d0152495
noy3928@naver.com <noyechan@noyechan-ui-MacBook-Pro.local>
2021-06-02 23:50:52 +0900

    이미지 클릭시 새창 띄우기

--- static/JS/app.js
+++ static/JS/app.js
@@ -1,38 +1,10 @@
 "use strict";
 
-let mainweather = bodymovin.loadAnimation({
-  container: document.querySelector(".weather-icon"),
-  renderer: "svg",
-  loop: true,
-  autoplay: true,
-  path: "./static/JS/animatedIcon/500.json",
-});
-
-let descWeather = bodymovin.loadAnimation({
-  container: document.querySelector(".desc-icon"),
-  renderer: "svg",
-  loop: true,
-  autoplay: true,
-  path: "./static/JS/animatedIcon/500.json",
-});
-
-//해당되는 클래스를 배열로 만들어줌.
-const weaklyWeatherIcon = $(".weakly-icon").toArray();
-
-//애니메이션을 동작하게 하는 함수 표현 / 아이콘 넣는 함수.
-const activeWeatherIcon = function (weatherCondition, DayN) {
-  let weeklyWeather = bodymovin.loadAnimation({
-    container: weaklyWeatherIcon[DayN],
-    renderer: "svg",
-    loop: true,
-    autoplay: true,
-    path: `./static/JS/animatedIcon/${weatherCondition}.json`,
-  });
-};
-
-const weatherInfo = [800, 800, 800, 800, 800, 800, 800];
-
-weatherInfo.forEach((a, i) => {
-  activeWeatherIcon(a, i);
-});
-// showResult();
+function getURL(place, food) {
+  window.open(
+    `https://www.google.com/maps/search/${place}+${food}/@37.5037227,127.0150586,14z/data=!3m1!4b1`,
+    _blank,
+    (channelmode = yes | no | 1 | 0),
+    false
+  );
+}
