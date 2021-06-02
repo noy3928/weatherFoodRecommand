d66321e4fb86393beb47c17cbe6c146afbcadb8b
noy3928@naver.com <noyechan@noyechan-ui-MacBook-Pro.local>
2021-06-02 21:58:01 +0900

    주소반환성공

--- static/JS/app.js
+++ static/JS/app.js
@@ -35,8 +35,5 @@ const weatherInfo = [800, 800, 800, 800, 800, 800, 800];
 weatherInfo.forEach((a, i) => {
   activeWeatherIcon(a, i);
 });
-<<<<<<< HEAD
-=======
 
 showResult();
->>>>>>> upstream/main
