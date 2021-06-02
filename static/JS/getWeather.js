b450dbcf1a3a1545a94159d1ae82b53a3443b9de
SeungBinKim <seungbin0508@gmail.com>
2021-06-02 19:46:09 +0900

    Changed getWeather to use Promise.

--- static/JS/getWeather.js
+++ static/JS/getWeather.js
@@ -2,13 +2,17 @@ function getWeather(coords) {
 	const API_KEY = "d0a4465b7d153b885b025b667926fff2"
 	const url = 'https://api.openweathermap.org/data/2.5/weather'
 	const query = `?lat=${coords.latitude}&lon=${coords.longitude}&appid=${API_KEY}&units=metirc`
-	return fetch(url+query)
-	.then(response => response.json())
-	.then(json => {
-		const temp = String(json.main.temp).slice(0, -1)
-		const condition = json.weather['0'].id
-		return {
-			temp, condition
-		}
+
+	return new Promise((resolve, reject) => {
+		fetch(url+query)
+		.then(response => response.json())
+		.then(json => {
+			const temp = String(json.main.temp).slice(0, -1)
+			const condition = json.weather['0'].id
+			resolve( {
+				temp, condition
+			})
+		})
 	})
+
 }
