4fda3ad0e4ba98a1c389016a59b83a23257189ef
noy3928@naver.com <noyechan@noyechan-ui-MacBook-Pro.local>
2021-06-02 14:15:58 +0900

    get lon&lat and get lon&lat by keyword

--- static/JS/getCurrentLC.js
+++ static/JS/getCurrentLC.js
@@ -23,6 +23,19 @@ function getCurrentLocation() {
         weatherLcInfo.plc = json.name + ", " + json.sys.country;
         weatherLcInfo.condition = json.weather["0"].id;
 
+        // 현재 위치의 위도와 경도로 한글로된 지명 불러오기.
+        $.ajax({
+          method: "GET",
+          url: `https://dapi.kakao.com/v2/local/geo/coord2address.json?x=${lat}&y=${lon}&input_coord=WGS84`,
+          // url: `https://dapi.kakao.com/v2/local/geo/coord2address.json?x=127.423084873712&y=37.0789561558879&input_coord=WGS84`,
+          data: {},
+          headers: {
+            Authorization: "KakaoAK 01c0cbd4fde85e34af273552852f8ebe",
+          },
+        }).done(function (msg) {
+          console.log(msg);
+        });
+
         console.log(
           weatherLcInfo.condition,
           weatherLcInfo.plc,
@@ -69,3 +82,26 @@ function getCurrentLocation() {
 }
 
 getCurrentLocation();
+
+function getLatLonbyKeyword() {
+  //키워드 입력시 위치 정보 불러오는 api
+  const searchLatLon = function () {
+    $.ajax({
+      method: "GET",
+      // url: `https://dapi.kakao.com/v2/local/geo/coord2address.json?x=${lat}&y=${lon}&input_coord=WGS84`,
+      url: "https://dapi.kakao.com/v2/local/search/keyword.json?y=37.514322572335935&x=127.06283102249932&radius=20000",
+      data: { query: $("#searchbar").val() },
+      headers: {
+        Authorization: "KakaoAK 01c0cbd4fde85e34af273552852f8ebe",
+      },
+    }).done(function (msg) {
+      console.log(msg);
+      // console.log($("#searchbar").val());
+    });
+  };
+
+  const searchForm = document.querySelector("form.search");
+  searchForm.addEventListener("submit", searchLatLon);
+}
+
+getLatLonbyKeyword();
