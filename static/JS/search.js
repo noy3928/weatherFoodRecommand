afbf5a03f4dd35a9804d94bd63908a32cdaf2b93
SeungBinKim <seungbin0508@gmail.com>
2021-06-03 16:07:50 +0900

    Completed search.js upto getting weather information.

--- static/JS/search.js
+++ static/JS/search.js
@@ -1,11 +1,14 @@
 async function search(e) {
 	console.log('function search starts')
-	console.log(e)
 
 	const coords = e?.type === 'submit'
 	? await korToCoords()
 	: getCurrentCoords()
 
-	console.log(coords)
+	console.log('현재 좌표입니다.', coords)
+
+	const weather = await getWeather(coords)
+	console.log('현재 날씨입니다.', weather)
+
 	console.log('function search ends')
 }
