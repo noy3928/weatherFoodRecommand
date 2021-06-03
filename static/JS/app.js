ae4e8572ed22ad0070f505ecfa3bed68462ec851
noy3928@naver.com <noyechan@noyechan-ui-MacBook-Pro.local>
2021-06-03 09:28:09 +0900

    음식클릭시 구글지도이동

--- static/JS/app.js
+++ static/JS/app.js
@@ -1,10 +1,9 @@
 "use strict";
-
-function getURL(place, food) {
-  window.open(
-    `https://www.google.com/maps/search/${place}+${food}/@37.5037227,127.0150586,14z/data=!3m1!4b1`,
-    _blank,
-    (channelmode = yes | no | 1 | 0),
-    false
-  );
+const features =
+  "menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes";
+// let plc = $("#searchbar").val();
+function geturl(place, food) {
+  // let url = `https://www.google.com/maps/search/${place}+${food}/@37.5037201,127.0150586,14z/data=!3m1!4b1`;
+  let url = `https://www.google.com/maps/search/%EC%A2%85%EB%A1%9C+%EB%8F%88%EA%B9%8C%EC%8A%A4/@37.5697172,126.9857489,17z/data=!3m1!4b1`;
+  let other = window.open(url, "_blank", features);
 }
