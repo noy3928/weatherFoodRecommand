7c427f4d0d6a324241078a2b2a31bd70d84e0264
noy3928@naver.com <noyechan@noyechan-ui-MacBook-Pro.local>
2021-06-03 15:29:50 +0900

    get Lat,Lng for map

--- static/JS/getCurrentLC.js
+++ static/JS/getCurrentLC.js
@@ -1,21 +1,27 @@
 function getCurrentCoords() {
-	const COORDS = "coords"
+  const COORDS = "coords";
 
-	function loadCoords() {
-		const loadedCords = JSON.parse(localStorage.getItem(COORDS)) ??	(function() {
-			return navigator.geolocation.getCurrentPosition(position => { // handleGeoSucees
-				const {latitude, longitude} = position.coords
-				const coordsObj = {latitude, longitude}
-				localStorage.setItem(COORDS, JSON.stringify(coordsObj))
-				return coordsObj
-			},
-			() => { // handleGeoError
-				console.error("Can't access geo location.")
-			})
-		})()
-		return loadedCords
-	}
-	return loadCoords()
+  function loadCoords() {
+    const loadedCords =
+      JSON.parse(localStorage.getItem(COORDS)) ??
+      (function () {
+        return navigator.geolocation.getCurrentPosition(
+          (position) => {
+            // handleGeoSucees
+            const { latitude, longitude } = position.coords;
+            const coordsObj = { latitude, longitude };
+            localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+            return coordsObj;
+          },
+          () => {
+            // handleGeoError
+            console.error("Can't access geo location.");
+          }
+        );
+      })();
+    return loadedCords;
+  }
+  return loadCoords();
 }
 
 function getLatLonbyKeyword() {
@@ -40,9 +46,11 @@ function getLatLonbyKeyword() {
 }
 
 function createKakaoMap(lat, lon) {
+  const { latitude, longitude } = getCurrentCoords();
+
   let mapContainer = document.querySelector(".kakao-map"), // 지도를 표시할 div
     mapOption = {
-      center: new kakao.maps.LatLng(lat, lon), // 지도의 중심좌표
+      center: new kakao.maps.LatLng(latitude, longitude), // 지도의 중심좌표
       level: 3, // 지도의 확대 레벨
       mapTypeId: kakao.maps.MapTypeId.ROADMAP, // 지도종류
     };
@@ -52,7 +60,7 @@ function createKakaoMap(lat, lon) {
 
   // 지도에 마커를 생성하고 표시한다
   var marker = new kakao.maps.Marker({
-    position: new kakao.maps.LatLng(lat, lon), // 마커의 좌표
+    position: new kakao.maps.LatLng(latitude, longitude), // 마커의 좌표
     map: map, // 마커를 표시할 지도 객체
   });
 }
