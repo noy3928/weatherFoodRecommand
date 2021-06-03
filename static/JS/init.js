d2891907dd517408dd7a65e2609c55f5aac78e5f
SeungBinKim <seungbin0508@gmail.com>
2021-06-03 15:10:34 +0900

    Added search.js.

--- static/JS/init.js
+++ static/JS/init.js
@@ -2,8 +2,6 @@ async function init() {
 	const consoleTitleStyle = 'background: #222; color: #bada55; font-size: xx-large;'
 	await console.log('%c페이지가 로드되었습니다.', consoleTitleStyle)
 
-	function search() {
-
-	}
+	search()
 
 }
