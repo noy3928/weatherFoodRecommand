6b63755249da2b890706a7f23b9b780147a2be43
mn00149 <mn00149@naver.com>
2021-06-05 23:15:35 +0900

    7일 날씨 예보함수 수정

--- static/API/weather_api.js
+++ static/API/weather_api.js
@@ -1,4 +1,4 @@
- var curr_weather
+
 
         function weather_info(lat, lon) {
 
@@ -13,7 +13,7 @@
 
                     console.log('현제기온:', temp, '날씨id:', condition)
 
-                    return curr_weather = {
+                    return {
                         "temp": temp,
                         "condition": condition
                     }
@@ -33,20 +33,20 @@
                 url: apiurl,
                 data: {},
                 success: function (response) {
-                    day1 = response['daily'][1]['weather'][0]['id'];
-                    day2 = response['daily'][2]['weather'][0]['id'];
-                    day3 = response['daily'][3]['weather'][0]['id'];
-                    day4 = response['daily'][4]['weather'][0]['id'];
-                    day5 = response['daily'][5]['weather'][0]['id'];
-                    day6 = response['daily'][6]['weather'][0]['id'];
-                    day7 = response['daily'][7]['weather'][0]['id'];
-                    temp1 = response['daily'][1]['temp']['day'] - 273;
-                    temp2 = response['daily'][2]['temp']['day'] - 273;
-                    temp3 = response['daily'][3]['temp']['day'] - 273;
-                    temp4 = response['daily'][4]['temp']['day'] - 273;
-                    temp5 = response['daily'][5]['temp']['day'] - 273;
-                    temp6 = response['daily'][6]['temp']['day'] - 273;
-                    temp7 = response['daily'][7]['temp']['day'] - 273;
+                    day1 = response['daily'][0]['weather'][0]['id'];
+                    day2 = response['daily'][1]['weather'][0]['id'];
+                    day3 = response['daily'][2]['weather'][0]['id'];
+                    day4 = response['daily'][3]['weather'][0]['id'];
+                    day5 = response['daily'][4]['weather'][0]['id'];
+                    day6 = response['daily'][5]['weather'][0]['id'];
+                    day7 = response['daily'][6]['weather'][0]['id'];
+                    temp1 = response['daily'][0]['temp']['day'] - 273;
+                    temp2 = response['daily'][1]['temp']['day'] - 273;
+                    temp3 = response['daily'][2]['temp']['day'] - 273;
+                    temp4 = response['daily'][3]['temp']['day'] - 273;
+                    temp5 = response['daily'][4]['temp']['day'] - 273;
+                    temp6 = response['daily'][5]['temp']['day'] - 273;
+                    temp7 = response['daily'][6]['temp']['day'] - 273;
 
 
                     console.log(day1, day2, day3, day4, day5, day6, day7)
