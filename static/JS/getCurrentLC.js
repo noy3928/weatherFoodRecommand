b6ee7aa9e1f83b76354e0f842a2afb0b5415b1af
SeungBinKim <seungbin0508@gmail.com>
2021-06-06 21:50:15 +0900

    Changed geturl() to work.

--- static/JS/getCurrentLC.js
+++ static/JS/getCurrentLC.js
@@ -30,6 +30,6 @@ function geturl(place, food) {
     "menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes";
   // let plc = $("#searchbar").val();
   // let url = `https://www.google.com/maps/search/${place}+${food}/@37.5037201,127.0150586,14z/data=!3m1!4b1`;
-  let url = `https://www.google.com/maps/search/%EC%A2%85%EB%A1%9C+%EB%8F%88%EA%B9%8C%EC%8A%A4/@37.5697172,126.9857489,17z/data=!3m1!4b1`;
+  let url = `https://www.google.com/maps/search/${place}+${food.name}`;
   let other = window.open(url, "_blank", features);
 }
